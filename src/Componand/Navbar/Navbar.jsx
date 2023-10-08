b2351f39ca4e0d34f2323ddd268efb21b9240479
towsifmah <mahmudtarak24@gmail.com>
2023-10-09 05:36:41 +0600

    siam vai commit

--- src/Componand/Navbar/Navbar.jsx
+++ src/Componand/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import {FaBars} from 'react-icons/fa'
 
 const Navbar = () => {
   const [show , notshow] = useState(false)
+  const hide = show ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'auto'
   const controlbar = () =>{
     notshow(!show)
   }
@@ -16,11 +17,16 @@ const Navbar = () => {
         </div>
          <div onClick={controlbar} className="lg:hidden text-[25px] font-semibold text-[#4D4D4D] border-2 border-[#4CAF4F] rounded ml-[50px] sm:ml-[328px] md:ml-[458px]">
           {
-            show === true ? <MdClose/> : <FaBars/>
+            show ? <MdClose/> : <FaBars/> 
           }
+          <div onClick={controlbar} className="hidden"> 
+          {
+            show ? hide : ''
+          }
+          </div>
          </div>
-        <div className={`lg:flex items-center gap-x-[101.5px] lg:static absolute lg:bg-transparent bg-[#4CAF4F]/70 backdrop-blur-sm duration-500 pb-[5%] lg:pb-0 left-[0px] text-center w-full ${show ? 'top-[66px]' : 'top-[-218px] sm:top-[-254px]'}`}>
-          <ul className="lg:flex items-center xl:gap-x-[50px] gap-x-[31px] text-white md:text-[#4D4D4D]">
+        <div className={`lg:flex items-center gap-x-[101.5px] lg:static fixed overflow-y-auto lg:overflow-visible lg:bg-transparent bg-[#4CAF4F]/70 backdrop-blur-sm  lg:backdrop-blur-none duration-500 ease-in-out lg:duration-0 lg:pb-0 py-20 lg:py-0 top-[50px] right-0 text-center h-screen lg:h-fit ${show ? 'w-1/2' : 'w-0 lg:w-full opacity-50 lg:opacity-100'}`}>
+          <ul className="flex flex-col lg:flex-row items-center xl:gap-x-[50px] gap-y-[50px] gap-x-[31px] text-white md:text-[#4D4D4D]">
             <li><a className='text-base font-medium font-Inter' href="">Home</a></li>
             <li><a className='text-base font-normal font-Inter' href="">Service</a></li>
             <li><a className='text-base font-normal font-Inter' href="">Feature</a></li>
@@ -28,7 +34,7 @@ const Navbar = () => {
             <li><a className='text-base font-normal font-Inter' href="">Testimonial</a></li>
             <li><a className='text-base font-normal font-Inter' href="">FAQ</a></li>
           </ul>
-        <div className="mt-[3%] lg:mt-0">
+        <div className="mt-[3%] lg:mt-0 ">
           <a href="" className='py-[10px] px-[20px] text-[14px] font-Inter font-medium bg-white rounded text-[#4CAF4F] mr-[14px]'>login</a>
           <a href="" className='py-[10px] px-[20px] text-[14px] font-Inter font-medium bg-[#4CAF4F] text-white rounded'>Sign up</a>
         </div>
--- src/Componand/Tim/Tim.jsx
+++ src/Componand/Tim/Tim.jsx
@@ -4,10 +4,10 @@ const Tim = () => {
     <div className="bg-[#F5F7FA] py-[32px] lg:px-[144px] px-[20px]">
         <div className="container">
           <div className="lg:flex gap-[78px]">
-          <div className='flex justify-center'>
+          <div className='lg:flex justify-center'>
                 <img src="src/assets/timImage.svg" alt="image" />
             </div>
-            <div className="lg:max-w-[748px] max-w-[620px]">
+            <div className="xl:max-w-[748px] w-full">
                 <p className="lg:text-base text-[12px] font-Inter font-medium text-[#717171] lg:leading-[24px]">Maecenas dignissim justo eget nulla rutrum molestie. Maecenas lobortis sem dui, vel rutrum risus tincidunt ullamcorper. Proin eu enim metus. Vivamus sed libero ornare, tristique quam in, gravida enim. Nullam ut molestie arcu, at hendrerit elit. Morbi laoreet elit at ligula molestie, nec molestie mi blandit. Suspendisse cursus tellus sed augue ultrices, quis tristique nulla sodales. Suspendisse eget lorem eu turpis vestibulum pretium. Suspendisse potenti. Quisque malesuada enim sapien, vitae placerat ante feugiat eget. Quisque vulputate odio neque, eget efficitur libero condimentum id. Curabitur id nibh id sem dignissim finibus ac sit amet magna.</p>
                     <h5 className="lg:text-[20px] text-[14px] font-semibold font-Inter text-[#4CAF4F] mt-[16px] mb-[8px]">Tim Smith</h5>
                 <p className="lg:text-base text-[12px] font-normal font-Inter text-[#89939E]">British Dragon Boat Racing Association</p>
